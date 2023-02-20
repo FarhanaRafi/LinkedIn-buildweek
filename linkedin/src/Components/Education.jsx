a8192445ec3b932fc4bb774bfc1b1eed6ec0ba95
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-02-20 20:35:16 +0100

    done

--- linkedin/src/Components/Education.jsx
+++ linkedin/src/Components/Education.jsx
@@ -6,7 +6,7 @@ const Education = () => {
     <>
       <div>
         <Card className="mt-3">
-          <Card.Body style={{ height: "65vh" }}>
+          <Card.Body style={{ height: "73vh" }}>
             <Card.Text>
               <h5 className="mb-3">
                 <strong>Education</strong>
@@ -25,7 +25,7 @@ const Education = () => {
                   <p className="ml-4 mt-n2">
                     Web Development Bootcamp
                     <br />
-                    Nov 2022Nov 2022
+                    Nov 2022
                     <br />
                     Front end development using html, css, java script,
                     bootstrap, reactJS, redux etc.. Backend development using
@@ -43,6 +43,22 @@ const Education = () => {
                 </span>
               </p>
               <hr />
+              <h6>
+                <img
+                  src="https://images.cdn1.stockunlimited.net/preview1300/university-logo-design_1970415.jpg"
+                  alt="logo"
+                  height={30}
+                />
+                {"  "}
+                <strong>UNIVERSITY</strong>
+              </h6>
+              <p className="ml-4 mt-n2">
+                Master of Science -
+                <br />
+                <span className="text-muted">Aug 2017 - Apr 2019</span>
+                <br />
+                Grade:8.9
+              </p>
             </Card.Text>
           </Card.Body>
         </Card>
