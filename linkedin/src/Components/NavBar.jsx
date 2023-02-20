7c4efc99717d845817d9fc3054b8c45058dd8c2b
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2023-02-20 15:44:57 +0100

    nav bar css

--- linkedin/src/Components/NavBar.jsx
+++ linkedin/src/Components/NavBar.jsx
@@ -7,7 +7,7 @@ import {
 } from "react-icons/bs";
 import { BiMessageRoundedDots, BiSearchAlt2 } from "react-icons/bi";
 import { TbGridDots } from "react-icons/tb";
-import { Form, InputGroup } from "react-bootstrap";
+import { Button, Dropdown, Form, InputGroup } from "react-bootstrap";
 
 const NavBar = () => {
   return (
@@ -28,30 +28,70 @@ const NavBar = () => {
         />
       </InputGroup>
       <div className="icon-link-section">
-        <a href="/">
+        <a href="/" className="links">
           <BsHouseDoorFill />
           Home
         </a>
-        <a href="/">
+        <a href="/" className="links">
           <BsPeopleFill />
           My Network
         </a>
-        <a href="/">
+        <a href="/" className="links">
           <BsBriefcaseFill />
           Jobs
         </a>
-        <a href="/">
+        <a href="/" className="links">
           <BiMessageRoundedDots />
           Messaging
         </a>
-        <a href="/">
+        <a href="/" className="links">
           <BsBellFill />
           Notifications
         </a>
-        <a href="/">Me</a>
+        <Dropdown>
+          <Dropdown.Toggle id="dropdown-basic" className="links">
+            <img
+              src="https://picsum.photos/200"
+              alt="profile"
+              className="profile-picture"
+            />
+            Me
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu alignRight>
+            <div className="dropdown-top">
+              <img
+                src="https://picsum.photos/200"
+                alt="profile"
+                className="dropdown-profile-picture"
+              />
+              <p>
+                <span>Eleni Kellermann</span> <br />
+                Student at EPICODE
+              </p>
+            </div>
+            <Dropdown.Item href="#/action-1" className="view-profile-button">
+              View Profile{" "}
+            </Dropdown.Item>
+            <hr />
+            <h5>Account</h5>
+            <Dropdown.Item href="#/action-3">
+              Try Premium for free
+            </Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Settings & Privacy</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Help</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Language</Dropdown.Item>
+            <hr />
+            <h5>Manage</h5>
+            <Dropdown.Item href="#/action-2">Posts & Activity</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Job Posting Account</Dropdown.Item>
+            <hr />
+            <Dropdown.Item href="#/action-2">Sign Out</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
       </div>
       <div className="right-nav-section">
-        <a href="/">
+        <a href="/" className="links">
           <TbGridDots />
           Work
         </a>
