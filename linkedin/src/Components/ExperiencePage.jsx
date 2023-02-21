f8cae51ffe1e36bae71e83dac071eb5b8f2ec14f
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-02-21 13:58:31 +0100

    done

--- linkedin/src/Components/ExperiencePage.jsx
+++ linkedin/src/Components/ExperiencePage.jsx
@@ -4,20 +4,33 @@ import { BsPlusLg } from "react-icons/bs";
 import { FiEdit2 } from "react-icons/fi";
 import ExperienceModal from "./ExperienceModal";
 import { useSelector } from "react-redux";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getExperiencesAsync } from "../Redux/Actions";
 
 const ExperiencePage = () => {
   const profiles = useSelector((state) => state.profiles.profiles);
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const id = useSelector((state) => state.profile.data);
+  const experienceFromRedux = useSelector(
+    (state) => state.experience.experiences
+  );
+  console.log(experienceFromRedux, "exp");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getExperiencesAsync(id._id));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <>
       <Container>
         <Row>
           <Col xs={12} md={8} className="mt-3">
             <Card className="mt-3 mb-5">
-              <Card.Body style={{ height: "73vh" }}>
+              <Card.Body style={{ height: "100%" }}>
                 <Card.Text>
                   <div className="mb-3 d-flex  mr-auto">
                     <h5>Experience</h5>
@@ -34,64 +47,29 @@ const ExperiencePage = () => {
                     </span>
                   </div>
 
-                  <div>
-                    <span>
-                      <div className="d-flex ">
-                        <img
-                          src="https://d92mrp7hetgfk.cloudfront.net/images/sites/misc/EPICODE_resized/original.png?1651791536"
-                          alt="logo"
-                          height={25}
-                        />
-                        {"  "}
-                        <h6>EPICODE Global</h6>
-                        <span className="ml-auto">
-                          <FiEdit2 onClick={handleShow} />
+                  {experienceFromRedux.map((exp) => {
+                    return (
+                      <div>
+                        <span>
+                          <div className="ml-4 h6 d-flex">
+                            {"  "}
+                            <strong>{exp.role}</strong>
+                            <span className="ml-auto">
+                              <FiEdit2 />
+                            </span>
+                          </div>
+                          <div className="ml-4 mt-n2">
+                            {exp.company}, {exp.area}
+                            <br />
+                            {exp.startDate} -{exp.endDate}
+                            <br />
+                            {exp.description}{" "}
+                          </div>
                         </span>
+                        <hr />
                       </div>
-
-                      <div className="ml-4 mt-n2">
-                        Web Development Bootcamp
-                        <br />
-                        Nov 2022
-                        <br />
-                        Front end development using html, css, java script,
-                        bootstrap, reactJS, redux etc.. Backend development
-                        using node js, mongo etc..Front end development using
-                        html, css, java script, bootstrap, reactJS, redux etc..{" "}
-                        <span className="mr-auto text-muted ml-5">
-                          see more...
-                        </span>
-                        <p className="mt-4">
-                          {" "}
-                          <strong>Skills:</strong>
-                          Cascading Style Sheets (CSS) · HTML5 · Node.js ·
-                          MongoDB · Web Services API · AJAX · Bootstrap ·
-                          Redux.js · React.js · JavaScript · Presentation Skills
-                          · Teamwork
-                        </p>
-                      </div>
-                    </span>
-                  </div>
-                  <hr />
-                  <div className="d-flex">
-                    <img
-                      src="https://images.cdn1.stockunlimited.net/preview1300/university-logo-design_1970415.jpg"
-                      alt="logo"
-                      height={30}
-                    />
-                    {"  "}
-                    <strong>UNIVERSITY</strong>
-                    <span className="ml-auto">
-                      <FiEdit2 />
-                    </span>
-                  </div>
-                  <p className="ml-4 mt-n2">
-                    Master of Science -
-                    <br />
-                    <span className="text-muted">Aug 2017 - Apr 2019</span>
-                    <br />
-                    Grade:8.9
-                  </p>
+                    );
+                  })}
                 </Card.Text>
               </Card.Body>
             </Card>
