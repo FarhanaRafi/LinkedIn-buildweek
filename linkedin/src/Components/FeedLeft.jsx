d06e8846d6ec1021f7edca54f58d19fca33e85e6
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-02-24 11:14:41 +0100

    done

--- linkedin/src/Components/FeedLeft.jsx
+++ linkedin/src/Components/FeedLeft.jsx
@@ -1,9 +1,61 @@
 import React from "react";
-import { Modal } from "react-bootstrap";
+import { Modal, Card } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { BsFillBookmarkFill } from "react-icons/bs";
 
 const FeedLeft = () => {
+  const myData = useSelector((state) => state.profile.data);
   return (
     <>
+      <Card className="mt-4">
+        <Card.Img
+          variant="top"
+          src="https://images.unsplash.com/photo-1428908728789-d2de25dbd4e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+          style={{ height: "12vh" }}
+        />
+        <Card.Img
+          src={myData.image}
+          className="mt-n4"
+          style={{
+            borderRadius: "50%",
+            height: "60px",
+            width: "60px",
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+        />
+
+        <Card.Body style={{ height: "100%" }}>
+          <Card.Text>
+            <h6>
+              <strong>
+                {myData.name} {myData.surname}
+              </strong>
+              <span className="text-muted"> (She/Her)</span>
+            </h6>
+            <p className="mt-n1">{myData.title}</p>
+            <p className="mt-n1">
+              Front end development using html, css, java script, bootstrap,
+              reactJS, redux etc..
+            </p>
+            <hr />
+
+            <p className="ml-auto text-muted d-flex">
+              <small> connections</small>{" "}
+              <span className="text-primary ml-auto">300</span>
+            </p>
+            <p className="text-muted ml-auto d-flex mt-n2">
+              <small>Who's viewed your profile</small>
+              <span className="text-primary ml-auto">120</span>
+            </p>
+            <hr />
+            <p>
+              <BsFillBookmarkFill />
+              <strong className="text-muted"> My Items</strong>
+            </p>
+          </Card.Text>
+        </Card.Body>
+      </Card>
       <div className="sticky-side1">
         <Modal.Dialog className=" mt-4">
           <Modal.Body>
